031acf26084eeb90405a04265bb0a34e222272dd
Mila Podchasova <Pod4asova20@gmail.com>
2024-02-13 11:58:51 +0300

    Create testing_elements_locators.py

---
+++ testing_elements_locators.py
@@ -0,0 +1,5 @@
+class MainBannerLocators:
+    BUTTON_START_TRADING = (By.CSS_SELECTOR, "a.cc-banner__btn.btn.btn--darkText.js_signup")
+    BUTTON_TRY_DEMO = (By.CSS_SELECTOR, "a.cc-banner__btn.btn.btn--emptyblack.js_signup.hideXs")
+    BUTTON_CREATE_ACCOUNT = (By.CSS_SELECTOR, '[data-type*="background_banner_block_btn1"]')
+    BUTTON_TRY_DEMO_ACCOUNT = (By.CSS_SELECTOR, '[data-type*="background_banner_block_btn2"]')
