49ae2a040306f730c2d45333351234562c55a953
Mila Podchasova <Pod4asova20@gmail.com>
2024-04-15 23:57:08 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
-
 @pytest.fixture()
 def driver():
     service = Service(ChromeDriverManager().install())
