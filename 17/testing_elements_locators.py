a291c28065a895d225770b469eea810399784102
Mila Podchasova <Pod4asova20@gmail.com>
2024-05-06 17:06:38 +0300

    Create testing_elements_locators.py

---
+++ 17/testing_elements_locators.py
@@ -0,0 +1,6 @@
+class HowToStartTradingSignupLocators:
+    SIGNUP_FRAME = (By.CSS_SELECTOR, 'div.banner__item #lp_form_126_6')
+    SIGNUP_INPUT_EMAIL = (By.CSS_SELECTOR, '#lp_form_126_6-email > input')
+    SIGNUP_INPUT_PASSWORD = (By.CSS_SELECTOR, "#lp_form_126_6-pass > input")
+    BUTTON_SIGN_UP = (By.CSS_SELECTOR, "div.bannerForm button.btn.btn")
+    SIGNUP_PRIVACY_POLICY = (By.CSS_SELECTOR, "div.bannerForm div.reg-desc a[target='_blank']")
