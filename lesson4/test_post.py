f1560c66c71bc203130d0fef1c542e82cfb08279
Mila Podchasova <Pod4asova20@gmail.com>
2024-05-09 14:02:19 +0300

    Update test_post.py

--- lesson4/test_post.py
+++ lesson4/test_post.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+
 class TestDependens:
     @pytest.fixture(scope="session")
     def driver():
