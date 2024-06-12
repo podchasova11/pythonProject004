06698d615e17c7db8503e32ad7548ac42c762f2e
Mila Podchasova <Pod4asova20@gmail.com>
2024-06-12 21:19:07 +0300

    Update test_login_01.py

--- lesson4/test_login_01.py
+++ lesson4/test_login_01.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 class TestFormRegistration:
 
     def setup(self):
