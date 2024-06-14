4f559601d108f1c18874d368af9a70856e35c2c5
Mila Podchasova <Pod4asova20@gmail.com>
2024-06-14 08:58:44 +0300

    Create AssertClass.py

---
+++ 17/14-06-2024_pages/Elements/AssertClass.py
@@ -0,0 +1,12 @@
+    @allure.step('Checking that The Desktop "Trading page: is opened')
+    def assert_desktop_trading_page(self, d):
+        print(f"\n{datetime.now()}   3. Assert_v0")
+        platform_overview_btn_link = d.current_url
+        if platform_overview_btn_link == "https://capital.com/":
+            assert False, \
+                ('Bug#029. '
+                 'Expected result: The Desktop Trading page is opened '
+                 '\n'
+                 'Actual result: The Home page is opened ')
+        else:
+            print(f"{datetime.now()}   =>There is no bug")
