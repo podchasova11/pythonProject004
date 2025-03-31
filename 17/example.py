98678b964fbfdd8f2db236be7dd4797afe3fa5da
Mila Podchasova <Pod4asova20@gmail.com>
2025-03-31 18:19:24 +0300

    Create example.py

---
+++ 17/example.py
@@ -0,0 +1,20 @@
+# ! Enheritance
+
+class Parents:
+
+   nationality = "American"
+
+   def __init__(self):
+       self.family = "Jonson"  
+
+   def eat(self):
+       print("Eatting")
+
+   def groing_up(self):
+       print("Groing Up")
+
+Mom = Parents()
+Mom.groing_up()
+
+
+  
