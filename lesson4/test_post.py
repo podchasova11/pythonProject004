56da4ceb6cceb9560b261d1f916f38776c26e22e
Mila Podchasova <Pod4asova20@gmail.com>
2025-07-14 21:57:55 +0300

    Update test_post.py

--- lesson4/test_post.py
+++ lesson4/test_post.py
@@ -37,4 +37,44 @@ class TestDependens:
     # Здесь выполняются действия для удаления поста
 
         assert "post_deleted" in driver.page_source
+####
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestFormRegistration:
+
+    def setup(self):
+        print("Выполняюсь до теста")
+        self.service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=self.service)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    @pytest.mark.parametrize("username, password", [
+        ("admin", "admin123"),
+        ("admin123", "admin")
+        ]
+    )
+
+    def test_login(driver, username, password):
+        username_field = driver.find_element("xpath", "//input[@name='username']")
+        password_field = driver.find_element("xpath", "//input[@name='password']")
+        submit_button = driver.find_element("xpath", "//*[@type='submit']")
+
+        username_field.clear()
+        username_field.send_keys(username)
+
+        password_field.clear()
+        password_field.send_keys(password)
+
+        submit_button.click()
+        
+            def teardown(self):
+        self.driver.quit()
+        print("Выполняюсь после всего теста")
+
+        
 
